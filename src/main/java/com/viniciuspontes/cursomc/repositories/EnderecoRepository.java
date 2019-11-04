dfc9d9d4fe44a20349a30cf2e39f1c3f761bde3a
Vinícius Pontes <vinicius.pontes3@etec.sp.gov.br>
2019-11-04 19:20:06 -0300

    Personalizando o retorno da validação

---
+++ src/main/java/com/viniciuspontes/cursomc/repositories/EnderecoRepository.java
@@ -0,0 +1,11 @@
+package com.viniciuspontes.cursomc.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.viniciuspontes.cursomc.domain.Endereco;
+
+@Repository
+public interface EnderecoRepository extends JpaRepository<Endereco, Integer>{
+
+}
---
+++ src/main/java/com/viniciuspontes/cursomc/services/ClienteService.java
@@ -0,0 +1,24 @@
+package com.viniciuspontes.cursomc.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.viniciuspontes.cursomc.domain.Cliente;
+import com.viniciuspontes.cursomc.repositories.ClienteRepository;
+import com.viniciuspontes.cursomc.services.exceptions.ObjectNotFoundException;
+
+@Service
+public class ClienteService {
+	
+	@Autowired
+	private ClienteRepository repo;
+	
+	public Cliente find(Integer id) {
+		Optional<Cliente> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException(
+					"Objeto não encontrado! Id: " + id + ", Tipo: " + Cliente.class.getName()));
+	}
+	
+}
