e94a8378e8878a848cd7af1efd7b7d36d94ec02c
Vinícius Pontes <vinicius.pontes3@etec.sp.gov.br>
2021-06-27 00:14:52 -0300

    Produto e associação muitos para muitos

--- src/main/java/com/viniciuspontes/cursomc/CursomcApplication.java
+++ src/main/java/com/viniciuspontes/cursomc/CursomcApplication.java
@@ -1,6 +1,5 @@
 package com.viniciuspontes.cursomc;
 
-import java.util.ArrayList;
 import java.util.Arrays;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,13 +8,18 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.viniciuspontes.cursomc.domain.Categoria;
+import com.viniciuspontes.cursomc.domain.Produto;
 import com.viniciuspontes.cursomc.repositories.CategoriaRepository;
+import com.viniciuspontes.cursomc.repositories.ProdutoRepository;
 
 @SpringBootApplication
 public class CursomcApplication implements CommandLineRunner {
 	
 	@Autowired
 	private CategoriaRepository categoriaRepository;
+	
+	@Autowired
+	private ProdutoRepository produtoRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
@@ -27,7 +31,19 @@ public class CursomcApplication implements CommandLineRunner {
 		Categoria categoria1 = new Categoria(null, "Informática");
 		Categoria categoria2 = new Categoria(null, "Escritório");
 		
+		Produto produto1 = new Produto(null, "Computador", 2000.00);
+		Produto produto2 = new Produto(null, "Impressora", 800.00);
+		Produto produto3 = new Produto(null, "Mouse", 80.00);
+		
+		categoria1.getProdutos().addAll(Arrays.asList(produto1, produto2, produto3));
+		categoria2.getProdutos().addAll(Arrays.asList(produto2));
+		
+		produto1.getCategorias().addAll(Arrays.asList(categoria1));
+		produto2.getCategorias().addAll(Arrays.asList(categoria1, categoria2));
+		produto3.getCategorias().addAll(Arrays.asList(categoria1));
+		
 		categoriaRepository.saveAll(Arrays.asList(categoria1, categoria2));
+		produtoRepository.saveAll(Arrays.asList(produto1, produto2, produto3));
 	}
 
 }
