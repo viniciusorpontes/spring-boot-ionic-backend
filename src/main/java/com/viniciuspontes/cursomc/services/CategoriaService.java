35c2e4905bdeed7f3f92d88f3603f8d90370c3aa
Vinícius Pontes <vinicius.pontes3@etec.sp.gov.br>
2021-06-29 00:11:07 -0300

    Ajustes no endpoint /categorias/{id}

--- src/main/java/com/viniciuspontes/cursomc/services/CategoriaService.java
+++ src/main/java/com/viniciuspontes/cursomc/services/CategoriaService.java
@@ -7,17 +7,18 @@ import org.springframework.stereotype.Service;
 
 import com.viniciuspontes.cursomc.domain.Categoria;
 import com.viniciuspontes.cursomc.repositories.CategoriaRepository;
+import com.viniciuspontes.cursomc.services.exceptions.ObjectNotFoundException;
 
 @Service
 public class CategoriaService {
 
-	
 	@Autowired
 	private CategoriaRepository categoriaRepository;
-	
+
 	public Categoria buscar(Integer id) {
 		Optional<Categoria> categoria = categoriaRepository.findById(id);
-		return categoria.orElse(null);
+		return categoria.orElseThrow(() -> new ObjectNotFoundException(
+				"Objeto não encontrado! Id: " + id + " , Tipo: " + Categoria.class.getName()));
 	}
-	
+
 }
