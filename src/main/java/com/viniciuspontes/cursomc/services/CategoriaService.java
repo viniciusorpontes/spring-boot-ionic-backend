f318c7ae839d37382ed817365f9586c477515c79
Vinícius Pontes <vinicius.pontes3@etec.sp.gov.br>
2021-10-01 12:14:06 -0300

    Atualizando Categoria com PUT, ouutros pequenos ajustes

--- src/main/java/com/viniciuspontes/cursomc/services/CategoriaService.java
+++ src/main/java/com/viniciuspontes/cursomc/services/CategoriaService.java
@@ -15,7 +15,7 @@ public class CategoriaService {
 	@Autowired
 	private CategoriaRepository categoriaRepository;
 
-	public Categoria buscar(Integer id) {
+	public Categoria find(Integer id) {
 		Optional<Categoria> categoria = categoriaRepository.findById(id);
 		return categoria.orElseThrow(() -> new ObjectNotFoundException(
 				"Objeto não encontrado! Id: " + id + " , Tipo: " + Categoria.class.getName()));
@@ -23,6 +23,11 @@ public class CategoriaService {
 	
 	public Categoria insert(Categoria categoria) {
 		categoria.setId(null);
+		return categoriaRepository.save(categoria);
+	}
+	
+	public Categoria update(Categoria categoria) {
+		find(categoria.getId());
 		return categoriaRepository.save(categoria);
 	}
 
